90c23805b9eda0bb8fe038669b97b07d5099126b
nonokaM <c1201071@gmail.com>
2023-10-31 00:37:51 +0900

    add: todosHandler

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/todos", todosHandler)
+	http.HandleFunc("/todos/", todosIdHandler)
+}
+
+func todosHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getAllTodos(w, r)
+	case "POST":
+		addNewTodo(w, r)
+	}
+}
+
+func todosIdHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getAllTodos() {
+
+}
+
+func addNewTodo() {
 	
 }
